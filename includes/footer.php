39276bd0c8f1da65283f00b6f07172bb912c88b7
UMAR ABID <57565130+umar-abid06@users.noreply.github.com>
2020-08-22 17:44:15 +0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <div class="container">
-		<div class="row bg-primary">
-			<h4 class="text text-center"> &copy; Copyrights 2020</h4>
+		<div class="row bg-info"style="background-color:black;color:white;">
+			<h3 class="text text-center"> &copy; Copyrights 2020</h3>
+			
 		</div>
 	</div>

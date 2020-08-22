7fc4edb5f34f6bca2e73dc485e35baf9fdd0093c
UMAR ABID <57565130+umar-abid06@users.noreply.github.com>
2020-08-22 17:39:55 +0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,8 +1,21 @@
 <?php
-include("includes/loginheader.php")
+session_start();
+include("includes/loginheader.php");
+if (!$_SESSION['user_email']) {
+   header('location:login.php');
+}
 ?>
+<br>
 <div class="container">
 <div class="col-sm-6">
-<h4 class="text text-center text-info bg-primary"> How to cast Your Vote?</h4>
+<h1 class="text text-center text-info bg-primary alert"> How to cast Your Vote?</h1>
+<ul style="font-size:1.8rem;">
+<li>Firstly Select <b>ID Generate</b> From navigation Bar</li>
+<li>Generate your ID to<b>VOTE</b>And Proceed Ahead with ELECTIONS</li>
+
+</ul>
+<div class="col-sm-6">
+<img src="images/1.jpg" class="img img-responsive" alt="error loading">
+
 </div>
 </div>

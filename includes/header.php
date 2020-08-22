680971a060bd2dc67fb921de176b3ff3929df05c
UMAR ABID <57565130+umar-abid06@users.noreply.github.com>
2020-08-22 17:41:57 +0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,33 +1,40 @@
-<html>
-<head>
-<title>Online Voting</title>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
-	<link rel="stylesheet" type="text/css" href="mystyle.css"/>
-	<link rel="stylesheet" type="text/css" href="css/fonts.css"/>
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<!DOCTYPE html>
+	<head>
+	<title>Online Voting</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+		<link rel="stylesheet" type="text/css" href="mystyle.css"/>
+		<link rel="stylesheet" type="text/css" href="css/fonts.css"/>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>
 
-</head>
-<body>
-	<div class="container">
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-				<a class="navbar-brand" href="#">ONLINE VOTING</a>
+	</head>
+<body style="background-image: url('images/imgimg1.jpg');">
+	
+
+		<div class="container">
+			<nav class="navbar navbar-inverse navbar-fixed top">
+				<div class="container-fluid">
+					<div class="navbar-header">
+					<a class="navbar-brand" href="#">ELECTLEADERS</a>
+				</div>
+				<ul class="nav navbar-nav">
+				<li class="active"><a href="index.php">Home</a></li>
+				<li><a href="reg.php">Registration</a></li>
+				<li><a href="login.php">Login</a></li>
+				<li><a href="welcome.php">Login header</a></li>
+				</ul>
 			</div>
-			<ul class="nav navbar-nav">
-			<li class="active"><a href="index.php">Home</a></li>
-			<li><a href="reg.php">Registeration</a></li>
-			<li><a href="login.php">Login</a></li>
-			<li><a href="welcome.php">Login header</a></li>
-			</ul>
-		</div>
-	</nav>
-</div>
-<div class="container">
-	<div class="row">
-		<div class="col-sm-8 col-sm-offset-2 img-thumbnail" style="background-color:gray;">
-			<img src="images/vote.jpg" class="img img-responsive" />
-		</div>
-		</div>
+		</nav>
 	</div>
+	<div class="container">
+		<div class="row">
+			<div class="card" style="background-color:black;opacity:0.8;">
+				<h3 style="padding:100px 300px 100px 200px;color:white;">
+				<div class="col-sm-13">
+				<h2 class="text text-centre text-info alert bg-info">Vote Today With <span> ELECTLEADERS.com <span></h2>
+				<img class="img-rounded" src="https://media.giphy.com/media/24FM6nyhn0k9L4v038/giphy.gif" alt="">
+			</div>
+			</body>
+			</div>
+		</div>

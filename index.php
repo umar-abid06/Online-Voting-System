3c0d8611621408b9a71169733240ba18d46c966f
UMAR ABID <57565130+umar-abid06@users.noreply.github.com>
2020-08-22 17:34:06 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,29 +3,29 @@ include("includes/header.php");
 ?>
 	<br>
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-6">
-				<h4 class="text text-centre text-info alert bg-primary">Cast your vote from anywhere in the world with just few clicks</h4>
-				<ul>
-	<li>Exercise your voting rights quickly and easily at home or from the other side of the
-	world</li>
-	<li>Cast your ballot with any device withe access to the internet from anywhere in the world</li>
-	<li>The security of your ballot is maintained under the high security standards adhered to by
-	online voting web-application</li>
-	<li>Your vote remains anonymous as our system architecture strictly separated your personal data
-	from the electronic ballot.</li>
+		<div class="row" style="background-color:black;opacity:0.8;">
+			<div class="col-sm-13">
+				<h3 class="text text-centre text-info alert bg-info">Cast your vote from anywhere in the world with just few clicks</h3>
+				<h2 class="text text-centre text-info alert bg-info"><ul style="color:black;font-size:1.9rem;">
+	<li><b>Exercise your voting rights quickly and easily at home or from the other side of the
+	world</b></li>
+	<li><b>Cast your ballot with any device withe access to the internet from anywhere in the world</b></li>
+	<li><b>The security of your ballot is maintained under the high security standards adhered to by
+	online voting web-application</b></li>
+	<li><b>Your vote remains anonymous as our system architecture strictly separated your personal data
+	from the electronic ballot.</b></li></h2>
+	<img class="img img-responsive" src="https://media.giphy.com/media/2MvPcQqA77u9y/giphy.gif" alt="">
 </ul>
 </div>
-			<div class="col-sm-6">
-            	<img src="images/1.jpg" class="img img-responsive"/>
-			</div>
+			
            </div>
 
 	</div> <br>
 <?php
 include("includes/footer.php");
 ?>
-<script type="text/javascript" src="js/bootstrap.js"/>
-<script type="text/javascript" src="js/jquery.js"/>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
 </body>
 </html>
+

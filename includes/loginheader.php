3e858be1296e745db4bcdbc64a800d0093fda966
UMAR ABID <57565130+umar-abid06@users.noreply.github.com>
2020-08-23 17:12:33 +0500

    Update loginheader.php

--- includes/loginheader.php
+++ includes/loginheader.php
@@ -7,35 +7,41 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
 	<link rel="stylesheet" type="text/css" href="mystyle.css"/>
 	<link rel="stylesheet" type="text/css" href="css/fonts.css"/>
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/bootstrap.js"></script>
+	
+
 
 </head>
-<body>
+<body style="background-image: url('images/imgimg1.jpg');">
 <div class="container">
-		<nav class="navbar navbar-default">
+		<nav class="navbar navbar-inverse">
 			<div class="container-fluid">
 				<div class="navbar-header">
-				<a class="navbar-brand" href="#">ONLINE VOTING SYSTEM</a>
+				<a class="navbar-brand" href="#">ELECTLEADERS.com</a>
 			</div>
             <ul class="nav navbar-nav">
 			<li class="active"><a href="index.php">Home</a></li>
-			<li><a href="reg.php">Parties</a></li>
-			<li><a href="login.php">Election</a></li>
-			<li><a href="welcome.php">Results</a></li>
-            <li><a href="welcome.php">Vote</a></li>
-            <li><a href="welcome.php">Logout</a></li>
+			<li><a href="idgenerate.php">ID Generate</a></li>
+			<li><a href="elections.php">Election</a></li>
+			<li><a href="results.php">Results</a></li>
+            <li><a href="vote.php">Vote</a></li>
+            <li><a href="logout.php">Logout</a></li>
+			<li><a href=""><?php echo $_SESSION['user_name']; ?></a></li>
+			
+			
 			</ul>
 		</div>
 	</nav>
 </div>
 <div class="container">
+		<div class="row">
 <div class="row">
-<div class="col-sm-8 col-sm-offset-2 img-thumbnail" style="background-color:gray;">
-			<img src="images/vote.jpg" class="img img-responsive" />
-		</div>
-<img src="img/voting-banner.jpg" class="img img-thumbnail img-responsive">
-</div>
-</div>
+			<div class="card" style="background-color:black;opacity:0.8;">
+				<h3 style="padding:100px 300px 100px 200px;color:white;">
+				<div class="col-sm-13">
+				<h2 class="text text-centre text-info alert bg-info">Vote Today With <span> ELECTLEADERS <span></h2>
+				<img class="img-rounded" src="https://media.giphy.com/media/24FM6nyhn0k9L4v038/giphy.gif" alt="">
+			</div>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
 </body>
 </html>
